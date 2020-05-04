f26dce82b50fd0b132aeae8af5e79fa16d2e11ae
asokra01 <31860009+asokra01@users.noreply.github.com>
2020-05-04 23:34:55 +0300

    Add files via upload

--- TrainerAn.php
+++ TrainerAn.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
     <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ 
   
     <title>Ironsky Fitness</title>
     <link rel="shortcut icon" href="images/ironsky2.png" type="image/png">
@@ -23,7 +23,28 @@
     <link rel="stylesheet" type="text/css" href="css/support.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <!--===============================================================================================-->
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     
+   <script>
+	$(document).ready(function(){
+ 	 $("button.drop").click(function(){
+    	$("div.up").slideToggle();  	
+	});
+	});
+    </script>
+  
+   <script>
+	$(document).ready(function(){
+ 	 $("button.drop").click(function(){
+    	$("div.marg").slideToggle();  	
+	});
+	});
+    </script>
+
+  
+
+
+
     <script>
         $(function () 
         {
@@ -33,14 +54,18 @@
 
     <style type="text/css">
 	h4.logo {margin-bottom:0 }
-	.container-contact100{  padding-top: 5%;}
+	.container-contact100{  padding-top: 5%; }
 	.wrap-contact100{ width: 70%; font-size: 20px;  }
-	.contact100-form-title{ font-size: 35px; color: teal; }
+	.contact100-form-title{ font-size: 30px; color: teal; }
 	.contact100-form{ width: 100%;}
 	.welcome{ width:100%; text-align:left;  color: DarkGoldenRod; font-size: 22px;}
 	table, tr{  max-width: 100%; border-spacing: 2px 50px; border-collapse: separate;}
 	td{border: 2px solid teal;  padding: 0px 10px 20px 5px; background-color: WhiteSmoke;}
-	.marg{ padding-top: 80px;}
+	td.fill  {background-color: Chocolate; text-align:center;   width: 5%;}
+	td a{color: white;}
+	.drop{background: DarkSlateGray;  }
+	.marg{ padding-top: 80px; display: none;}
+	.ga{font-size: 40px; padding-top: 20px;}
     </style>
 </head>
 
@@ -56,49 +81,59 @@
     <div class="form-gap" style="background-color:#1E4072;">
         <h4 class="logo ml-4 text-white"> I R O N S K Y <br>  <span> FITNESS </span> </h4>
     </div>
-
+	
     <div class="container-contact100" style="background-color:#1E4072;">
-        <div class="wrap-contact100"  >
-	        
-		<form class="contact100-form validate-form" method="POST" action= "php/PostAnnouncements.php">
+         
+        <div class="row">
+        
+            <div class="col-lg-12 mx-auto">  
+	            <div class="card"  >
                
-		<span class="contact100-form-title">
-                    Announcements
-                </span>
-                <span align-text:left; style="font-size: 26px">
-                    Write Your Announcement
-                </span>
-                  <label class="label-input100" for="subject">Subject *</label>
-                <div class="wrap-input100 validate-input" data-validate="Subject is required">
-                    <input id="subject" class="input100" name="subject" placeholder="Write title of Subject">
-                    <span class="focus-input100"></span>
+	               <div class="up">
+		              <span class="contact100-form-title ga">Announcements</span> 
+		              <span class="contact100-form-title"> Write your Announcement </span>
+		
+		              <form class="contact100-form validate-form" method="POST" action= "php/PostAnnouncements.php">
+            
+		                
+                            <label class="label-input100" for="subject">Subject *</label>
+                            <div class="wrap-input100 validate-input" data-validate="Subject is required">
+                                <input id="subject" class="input100" name="subject" placeholder="Write title of Subject">
+                                <span class="focus-input100"></span>
+                            </div>
+
+                            <label class="label-input100" for="message">Message *</label>
+                            <div class="wrap-input100 validate-input" data-validate="Message is required">
+                                <textarea id="message" class="input100" name="message" placeholder="Write us a message"></textarea>
+                                <span class="focus-input100"></span>
+                            </div>
+
+                            <div class="container-contact100-form-btn">
+                                <button class="contact100-form-btn sub" type="submit" name="submit">Post Announcement</button>
+
+                            </div> 
+                    </form>
+	               </div>
+               
+                <button  class="contact100-form-btn drop" type="submit" name="submit">READ AND MODIFY THE POSTED ANNOUNCEMENTS</button>
+	       
+                <div class= "marg" >
+	           <span class="contact100-form-title"> All Posts </span>	
+	           <?php include("php/TrainerAn-PHP.php"); ?>
+	       </div>
+          
                 </div>
-
-                <label class="label-input100" for="message">Message *</label>
-                <div class="wrap-input100 validate-input" data-validate="Message is required">
-                    <textarea id="message" class="input100" name="message" placeholder="Write us a message"></textarea>
-                    <span class="focus-input100"></span>
-                </div>
-
-                <div class="container-contact100-form-btn">
-                    <button class="contact100-form-btn sub" type="submit" name="submit">
-                        Post Announcement
-                    </button>
-
-                </div> 
-            </form>
- 		
-       
-	<div class= "marg" >
-	<span class="contact100-form-title"> Client Announcements </span>	
-
-	<?php include("php/TrainerAn-PHP.php"); ?>
-	</div>
+	
+	       
+	
               
-    </div>
+	      
+             
+        </div>
 </div>
-        <div id="dropDownSelect1"></div>
 
+        <div id="dropDownSelect1"></div>
+     </div>
         <!--===============================================================================================-->
         <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
         <!--===============================================================================================-->
